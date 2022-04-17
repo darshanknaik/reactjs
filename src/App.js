0f22201ea98e10072bae64f67e567a4146872108
darshanknaik <dnaik6955@gmail.com>
2022-04-17 11:28:19 +0530

    props passing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import Darshan from './components/Darshan';
+
 
 function App() {
+  let name="darshan";
+
+  let title="naik"
   return (
     <>
     <div className="app">
      <h1>hello world</h1>
     </div>
+    <div>
+      <Darshan name={name} title={title} />
+    </div>
     </>
   );
 }
+
 
 export default App;
---
+++ src/components/Darshan.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Darshan(props) {
+  return (
+    <div> iam {props.name} kamalakar {props.title}  </div>
+  )
+}
+
+export default Darshan
